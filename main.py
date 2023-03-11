a9fe3cd06e165f47c8f80e6120bb377ac3b74631
semaan <toninsemaan@gmail.com>
2023-03-11 10:22:22 +0200

    initial commit: add code structure

---
+++ main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from api.controllers import health_check_controller
+
+app = FastAPI(version='1.0', title='Restaurant Management Backend',
+              description="Providing different services")
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   )
+
+app.include_router(
+    router=health_check_controller.router,
+    prefix="/health",
+    tags=["health"],
+)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="localhost", port=8000)
